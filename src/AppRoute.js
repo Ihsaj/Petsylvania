878bcadeba48c2eb3bb2633e453d520c7b9398d0
Espanol <edwardespanyol12@gmail.com>
2025-12-03 15:14:27 +0800

    Added AppRoute

---
+++ src/AppRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "./Pages/MainLayout";
+
+function AppRoute() {
+  return (
+    <>
+      <MainLayout />
+    </>
+  );
+}
+
+export default AppRoute;
